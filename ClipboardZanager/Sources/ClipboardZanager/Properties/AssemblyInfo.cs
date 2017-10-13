3f3ba6c17245e4960c096d2094b9e32f121d6936
Etienne BAUDOUX <ebaudoux@velersoftware.com>
2017-10-13 18:30:52 +0200

    Fixed an issue where the a UI component with no defined tooltip was displaying an empty tooltip instead of nothing.

--- ClipboardZanager/Sources/ClipboardZanager/Properties/AssemblyInfo.cs
+++ ClipboardZanager/Sources/ClipboardZanager/Properties/AssemblyInfo.cs
@@ -55,8 +55,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2017.9.19.7")]
-[assembly: AssemblyFileVersion("2017.9.19.7")]
+[assembly: AssemblyVersion("2017.10.13.2")]
+[assembly: AssemblyFileVersion("2017.10.13.2")]
 
 [assembly: InternalsVisibleTo("ClipboardZanager.Tests")]
 
