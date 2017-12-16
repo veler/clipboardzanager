35ecef3958f59b753fed7b1c2e149a459a86343c
Etienne BAUDOUX <ebaudoux@velersoftware.com>
2017-12-16 16:09:23 +0100

    Fixed an english mistake. (#60)

--- ClipboardZanager/Sources/ClipboardZanager/Properties/AssemblyInfo.cs
+++ ClipboardZanager/Sources/ClipboardZanager/Properties/AssemblyInfo.cs
@@ -55,8 +55,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2017.12.16.8")]
-[assembly: AssemblyFileVersion("2017.12.16.8")]
+[assembly: AssemblyVersion("2017.12.16.9")]
+[assembly: AssemblyFileVersion("2017.12.16.9")]
 
 [assembly: InternalsVisibleTo("ClipboardZanager.Tests")]
 
